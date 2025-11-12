1efdd396b7617de9fdb32bb45fa318ef7eeeec3f
Dustin Greenlee <dusting@nwmissouri.edu>
2025-11-12 10:23:20 -0600

    Added create page

--- routes/elephant.js
+++ routes/elephant.js
@@ -8,4 +8,7 @@ router.get('/', elephant_controller.elephant_View_all_Page);
 /* Get detail elephant page */
 router.get('/detail', elephant_controller.elephant_view_one_Page);
 
+/* Get create elephant page */
+router.get('/create', elephant_controller.elephant_create_Page);
+
 module.exports = router;
