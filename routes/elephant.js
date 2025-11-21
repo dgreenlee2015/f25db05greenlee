6910bccfc65bbb6fa5481fc610ea465dbb4d51c1
Dustin Greenlee <dusting@nwmissouri.edu>
2025-11-21 09:44:53 -0600

    Secured update endpoint

--- routes/elephant.js
+++ routes/elephant.js
@@ -2,6 +2,14 @@ var express = require('express');
 const elephant_controller = require('../controllers/elephantCollection');
 var router = express.Router();
 
+const secured = (req, res, next)=> {
+    if(req.user)
+    {
+        return next();
+    }
+    res.redirect("/login");
+}
+
 /* GET elephants. */
 router.get('/', elephant_controller.elephant_View_all_Page);
 
@@ -12,7 +20,7 @@ router.get('/detail', elephant_controller.elephant_view_one_Page);
 router.get('/create', elephant_controller.elephant_create_Page);
 
 /* Get create update page */
-router.get('/update', elephant_controller.elephant_update_Page);
+router.get('/update', secured, elephant_controller.elephant_update_Page);
 
 /* Get delete elephant page */
 router.get('/delete', elephant_controller.elephant_delete_Page);
